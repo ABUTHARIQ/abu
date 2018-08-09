df9b96a3e3a341b71d5ffae381aa107ca5ebc08b
ABUTHARIQ <42167193+ABUTHARIQ@users.noreply.github.com>
2018-08-09 14:13:23 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,6 @@
+a=input("enter the char")
+if((a=='a')or(a=='e')or(a=='i')or(a=='o')or(a=='u')or(a=='A')or(a=='E')or(a=='I')or(a=='O')or(a=='U')):
+    print("It is vowels")
+else:
+    print("It is consonant")
+      
