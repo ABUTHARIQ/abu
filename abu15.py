cb23e0fbeb51c6afe961234aa3a1e8d024cde18f
ABUTHARIQ <42167193+ABUTHARIQ@users.noreply.github.com>
2019-01-24 11:18:42 +0530

    Update abu15.py

--- abu15.py
+++ abu15.py
@@ -1,6 +1,6 @@
-a=input("enter the number:")
-b=input("enter the number:")
-c=input("enter the number:")
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=int(input("enter the number:"))
 if((a>b)and(a>c)):
     print(a)
 elif((b>a)and(b>c)):
