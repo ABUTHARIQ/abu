d94ee897523fe9cdd3e8e7285a11182943584180
ABUTHARIQ <42167193+ABUTHARIQ@users.noreply.github.com>
2019-01-24 11:04:43 +0530

    Create abu13.py

---
+++ abu13.py
@@ -0,0 +1,7 @@
+t=(input("enter character:"))
+if((t=='a')or(t=='e')or(t=='i')or(t=='o')or(t=='u')):
+       print("vowels")
+elif((t==('b')or(t=='c')or(t=='d')or(t=='f')or(t=='g')or(t=='h')or(t=='j')or(t=='k')or(t=='l')or(t=='m')or(t=='n')or(t=='p')or(t=='q')or(t=='r')or(t=='s')or(t=='t')or(t=='v')or(t=='w')or(t=='x')or(t=='y')or(t=='z'))):
+        print("consonant")
+else:
+        print(" It is invalid")
